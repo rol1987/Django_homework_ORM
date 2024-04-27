236b4abe8e03fc52eefc2f7669cde445f40ca976
Sergey <116816800+rol1987@users.noreply.github.com>
2024-04-27 10:41:41 +0300

    Первый коммит

---
+++ phones/management/commands/import_phones.py
@@ -0,0 +1,26 @@
+import csv
+
+from django.utils.text import slugify
+
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as file:
+            phones = list(csv.DictReader(file, delimiter=';'))
+
+            for item in phones:
+                Phone.objects.create(
+                    id=item['id'],
+                    name=item['name'],
+                    price=item['price'],
+                    image=item['image'],
+                    release_date=item['release_date'],
+                    lte_exists=item['lte_exists'],
+                    slug=slugify(item['name']))
+            Phone.save()
